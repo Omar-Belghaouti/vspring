efcf2c375a755b15a75d7509944f7c663ea1c01b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-04-30 23:41:48 +0100

    initial commit

---
+++ src/main/java/com/omarox/vspring/VspringApplication.java
@@ -0,0 +1,13 @@
+package com.omarox.vspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omarox/vspring/VspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarox.vspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
