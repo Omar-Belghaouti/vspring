7d5d7a4b249090da1ff98dc82e8f6050d5854385
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-05-01 21:15:20 +0100

    using derby with JPA

--- src/main/java/com/omarox/vspring/service/TopicService.java
+++ src/main/java/com/omarox/vspring/service/TopicService.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import com.omarox.vspring.dao.TopicRepository;
 import com.omarox.vspring.model.Topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
@@ -18,30 +23,24 @@ public class TopicService {
             new Topic("javascript", "JavaScript", "JavaScript Description")));
 
     public List<Topic> getAllTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Optional<Topic> getTopic(String id) {
-        return topics.stream()
-                .filter(t -> t.getId().equals(id))
-                .findFirst();
+        return topicRepository.findById(id);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        topics.stream()
-                .filter(t -> t.getId().equals(id))
-                .findFirst()
-                .ifPresent(t -> {
-                    t.setName(topic.getName());
-                    t.setDescription(topic.getDescription());
-                });
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
