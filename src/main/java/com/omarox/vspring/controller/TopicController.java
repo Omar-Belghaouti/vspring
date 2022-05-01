8eaa73504482a0b36c114afc5dfe870156fb1099
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-05-01 12:47:08 +0100

    update a topic

--- src/main/java/com/omarox/vspring/controller/TopicController.java
+++ src/main/java/com/omarox/vspring/controller/TopicController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,11 @@ public class TopicController {
     @PostMapping
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
+    }
+
+    @PutMapping("/{id}")
+    public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
+        topicService.updateTopic(id, topic);
     }
 
 }
